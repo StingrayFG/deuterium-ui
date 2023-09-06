45bd744286bf27bed98d8eb4c0120536bada6e83
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-09-06 15:47:10 +0300

    added link generation

--- app/components/FileUploadPage.jsx
+++ app/components/FileUploadPage.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 import api from 'api'
 
-function FileUploader() {
+export default function FileUploadPage() {
 
   const [file, setFile] = useState();
   const [link, setLink] = useState();
@@ -15,7 +15,7 @@ function FileUploader() {
     }
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (!file) {
       return;
     }
@@ -23,29 +23,35 @@ function FileUploader() {
     const formData = new FormData();
     formData.append('file', file);
 
-    fetch(api.baseUrl + '/files', {
+    await fetch(api.baseUrl + '/upload', {
       method: 'POST',
       body: formData    
     })
       .then(res => res.json())
       .then(data => (
-        console.log(data),
         setLink(data.link)
         ))
       .catch(err => console.error(err));
 
   };
 
-  return (
-    <div>
-      <input type='file' onChange={handleFileChange} />
-
-      <div>{file && `${file.name} - ${file.type}`}</div>
-
-      <button onClick={handleSubmit}>Upload</button>
-      <p>{link}</p>
-    </div>
-  );
+  if (!link) {
+    return (
+      <div>
+        <input type='file' onChange={handleFileChange} />
+  
+        <div>{file && `${file.name} - ${file.type}`}</div>
+  
+        <button onClick={handleSubmit}>Upload</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Your link:</p>
+        <p>{'http://localhost:3000/' + link}</p>
+      </div>
+    );
+  }
+  
 }
-
-export default FileUploader;
