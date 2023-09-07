cf05462005ca4e7c50d955764b996457585713b6
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-09-07 17:19:28 +0300

    added divs

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -3,5 +3,9 @@ import React from 'react';
 import FilePage from 'components/FilePage'
 
 export default function Page({ params }) {
-  return <FilePage slug={params.slug} />
+  return(
+    <div className='bg-gradient-to-b from-black/75 via-transparent to-sky-500/75 h-screen'>
+      <FilePage slug={params.slug} />
+    </div>
+  )
 }
--- app/page.js
+++ app/page.js
@@ -3,5 +3,10 @@ import React from 'react';
 import FileUploadPage from 'components/FileUploadPage'
 
 export default function Page() {
-  return <FileUploadPage />
+  return (
+    <div className='bg-gradient-to-b from-black/75 via-transparent to-sky-500/75 h-screen'>
+      <FileUploadPage /> 
+    </div>
+    
+  )
 }
