70eb02fb78cb20b96487192963be4832dbe234f4
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-09-08 19:58:21 +0300

    added new download button

--- app/components/FilePage.jsx
+++ app/components/FilePage.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Link from 'next/link'
 
 import api from 'api'
@@ -8,6 +9,7 @@ import api from 'api'
 export default function FilePage(props) {
 
   const [fileName, setFileName] = useState();
+  const [fileSize, setFileSize] = useState();
   const [exists, setExists] = useState();
   const [isFetched, setIsFetched] = useState();
 
@@ -18,7 +20,8 @@ export default function FilePage(props) {
         setIsFetched(true)
         setExists(res.data.exists);
         if (res.data.exists) {
-          setFileName(res.data.fileName)
+          setFileName(res.data.fileName);
+          setFileSize(res.data.fileSize);
         }
       })
       .catch(err => console.error(err));
@@ -31,22 +34,29 @@ export default function FilePage(props) {
     if (exists === true)
     {
       return (
-        <div>
-          <p>{fileName}</p>
-          <Link href={api.baseUrl + '/file/' + props.slug + '/download'}>Download</Link>
-        </div>
+        <div className='w-200 h-96 mx-auto'>
+          <p className='text-sky-400 font-sans text-2xl text-center pt-48'>{fileName} {fileSize}MB</p>
+          <Link href={api.baseUrl + '/file/' + props.slug + '/download'}>
+            <div className='w-96 h-32 mx-auto mt-8 
+              bg-gray-900 hover:bg-gray-800/75 active:bg-gray-700/75
+              border-solid border-2 border-sky-700 rounded-lg'>
+              <p className='text-center mt-9 text-sky-400 font-sans text-4xl'>Download</p>
+            </div>
+          </Link>
+          
+        </div> 
       );
     } else if (exists === false) {
       return (
-        <div>
-          <p>File does not exist</p>
-        </div>
+        <div className='w-200 h-96 mx-auto'>
+          <p className='text-sky-400 font-sans text-2xl text-center pt-48'>File does not exist</p>
+        </div> 
       );
     } else {
       return (
-        <div>
-          <p>Something went wrong</p>
-        </div>
+        <div className='w-200 h-96 mx-auto'>
+          <p className='text-sky-400 font-sans text-2xl text-center pt-48'>Something went wrong</p>
+        </div> 
       );
     }
   }
