4878004fc5992c739ac3ae9778d9000de6ed12dc
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-09-11 11:05:00 +0300

    changed the logo

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -4,13 +4,13 @@ import Image from 'next/image'
 import FilePage from 'components/FilePage'
 
 import githubMark from '../../images/github-mark-white.png';
-import deLogo from '../../images/de-logo-white.png';
+import deLogo from '../../images/logo-white.png';
 import Link from 'next/link';
 
 export default function Page({ params }) {
   return (
     <div className='bg-gradient-to-b from-black/75 via-transparent to-sky-500/75 h-screen'>
-      <Image src={deLogo} alt='logo' width={100} className='pl-4 pt-2 inline-grid'/>
+      <Image src={deLogo} alt='logo' width={128} className='pl-4 inline-grid'/>
       <FilePage slug={params.slug} />
       <Link href='https://github.com/StingrayFG/' className='absolute bottom-0 left-0 p-6'>
         <Image src={githubMark} alt='github' width={80} />
--- app/page.js
+++ app/page.js
@@ -4,13 +4,13 @@ import Image from 'next/image'
 import FileUploadPage from 'components/FileUploadPage'
 
 import githubMark from '../images/github-mark-white.png';
-import deLogo from '../images/de-logo-white.png';
+import deLogo from '../images/logo-white.png';
 import Link from 'next/link';
 
 export default function Page() {
   return (
     <div className='bg-gradient-to-b from-black/75 via-transparent to-sky-500/75 h-screen'>
-      <Image src={deLogo} alt='logo' width={100} className='pl-4 pt-2 inline-grid'/>
+      <Image src={deLogo} alt='logo' width={128} className='pl-4 inline-grid'/>
       <FileUploadPage /> 
       <Link href='https://github.com/StingrayFG/' className='absolute bottom-0 left-0 p-6'>
         <Image src={githubMark} alt='github' width={80} />
