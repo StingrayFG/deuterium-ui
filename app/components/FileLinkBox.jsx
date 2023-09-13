079b3aa07f32417da0d6c52c7044256c778fb608
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-09-13 13:33:07 +0300

    Update FileLinkBox.jsx

--- app/components/FileLinkBox.jsx
+++ app/components/FileLinkBox.jsx
@@ -13,7 +13,7 @@ export default function FilePage({progress, fileSize, link}) {
     return(
       <div className='w-3/5 h-12 mx-auto mt-8
         border-solid border-2 border-sky-700 rounded-lg'>
-        <p className='text-center mt-1 text-sky-200 font-sans text-2xl'>Something went wrong, try again later</p>
+        <p className='text-center mt-1 text-sky-200 font-sans text-2xl'>Something went wrong</p>
       </div>  
     )
   } else if (fileSize > 100) {
