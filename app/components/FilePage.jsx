2e8fdc3044332c5d708ea0c83d567abdb3701368
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-10-01 10:15:35 +0300

    added hashSum displaying to the download page

--- app/components/FilePage.jsx
+++ app/components/FilePage.jsx
@@ -4,68 +4,63 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Link from 'next/link'
 
+import HashSumBox from './HashSumBox';
+
 export default function FilePage(props) {
 
-  const [fileName, setFileName] = useState();
-  const [fileSize, setFileSize] = useState();
-  const [exists, setExists] = useState();
-  const [isFetched, setIsFetched] = useState();
+  const [fileData, setFileData] = useState();
   const [isFailed, setIsFailed] = useState();
 
   useEffect(() => {
     const getFile = async () => {
       await axios.get(process.env.NEXT_PUBLIC_BACKEND_URL  + '/file/' + props.slug)
       .then(res => {
-        setIsFetched(true)
-        setExists(res.data.exists);
-        if (res.data.exists) {
-          setFileName(res.data.fileName);
-          setFileSize(res.data.fileSize);
-        }
+        setFileData(res.data);
       })
       .catch(err => {
         console.error(err)
         setIsFailed(true)
       });
     };
-    getFile();
+    if (!fileData) {
+      getFile();
+    }
   });
 
-  if (isFetched === true)
+  if (fileData)
   {
-    if (exists === true)
+    if (fileData.exists === true)
     {
       return (
-        <div className='w-11/12 md:w-200 h-96 mx-auto place-self-center'>
-          <p className='text-sky-400 font-sans text-2xl text-center'>
-            {fileName} {fileSize}MB
-          </p>
-          <Link href={process.env.NEXT_PUBLIC_BACKEND_URL + '/file/' + props.slug + '/download'}>
-            <div className='w-11/12 h-24 md:h-32 mx-auto mt-8 grid
-              bg-gray-900 hover:bg-gray-800/75 active:bg-gray-700/75
-              border-solid border-2 border-sky-700 rounded-lg'>
-              <p className='text-center text-sky-400 font-sans text-2xl md:text-4xl place-self-center'>Download</p>
+        <div className='w-11/12 md:w-[50rem] h-auto mx-auto mb-12 md:mb-36 place-self-center'>
+          <p className='h-12 text-sky-400 font-sans text-center text-xl md:text-2xl'>Files up to 100 MB are allowed</p>
+          <div className='w-full h-auto mx-auto sliding-div self-center
+            bg-gray-900/50
+            border-dashed border-2 border-sky-200 rounded-lg'>
+            <Link href={process.env.NEXT_PUBLIC_BACKEND_URL + '/file/' + props.slug + '/download'}>
+              <div className='w-11/12 h-24 md:h-32 mx-auto mt-8 grid
+                bg-gray-900 hover:bg-gray-800/75 active:bg-gray-700/75
+                border-solid border-2 border-sky-700 rounded-lg'>
+                <p className='text-center text-sky-400 font-sans text-2xl md:text-4xl place-self-center'>Download</p>
+              </div>
+            </Link>
+            <div className='h-14 md:h-16 grid'>
+              <p className='pl-6 pr-6 mt-2 text-center align-middle text-sky-400 font-sans text-xl md:text-2xl self-center'>{fileData.name} {fileData.size}MB</p>
             </div>
-          </Link>
-          
-        </div> 
-      );
-    } else if (exists === false) {
-      return (
-        <div className='w-11/12 md:w-200 h-96 mx-auto place-self-center'>
-          <p className='text-sky-400 font-sans text-2xl text-center'>File does not exist</p>
+            <HashSumBox hashSum={fileData.hashSum}/>
+          </div>
         </div> 
       );
     } else {
       return (
-        <div className='w-11/12 md:w-200 h-96 mx-auto place-self-center'>
-          <p className='text-sky-400 font-sans text-2xl text-center'>Something went wrong</p>
+        <div className='w-11/12 md:w-[50rem] h-auto mx-auto place-self-center'>
+          <p className='text-sky-400 font-sans text-2xl text-center'>File does not exist</p>
         </div> 
       );
     }
-  } else if (isFailed){
+  } else if (isFailed) {
     return (
-      <div className='w-11/12 md:w-200 h-96 mx-auto place-self-center'>
+      <div className='w-11/12 md:w-[50rem] h-auto mx-auto place-self-center'>
         <p className='text-sky-400 font-sans text-2xl text-center'>Something went wrong</p>
       </div> 
     );
---
+++ app/components/HashSumBox.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from 'react';
+
+export default function HashSumBox({ hashSum }) {
+
+  const [fade, setFade] = useState();
+
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+
+  const copyHashSum = async () => {
+    navigator.clipboard.writeText(hashSum)
+    setFade(true);
+    await delay(2000);
+    setFade(false);
+  };
+
+  return(
+    <div onClick={copyHashSum} className='w-11/12 md:w-8/12 h-20 md:h-12 mt-4 mb-8 mx-auto grid
+      hover:bg-gray-800/50 active:bg-gray-700/50
+      border-solid border-2 border-sky-700 rounded-lg'>
+      <p className={`text-center text-sky-200 font-sans text-xl md:text-2xl ml-4 mr-4 place-self-center break-all
+        transition-all duration-500 ${fade ? "opacity-0" : "opacity-100"}`}>
+        md5: {hashSum}
+      </p>
+      <p className={`text-center text-sky-200 font-sans text-xl md:text-2xl place-self-center absolute
+        transition-all duration-500 ${fade ? "opacity-100" : "opacity-0"}`}>
+        Copied!
+      </p> 
+    </div>  
+  ) 
+  
+}
