32e6b526d41379d6872f2e4c8da3aad5144a91c0
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-10-01 10:12:51 +0300

    removed unused widths

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,10 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
+    extend: {    
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      width: {
-        '200': '50rem',
-        '184': '46rem',
       },
       height: {
         '88': '22rem',
