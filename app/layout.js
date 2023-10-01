87cff4d5ab26b8e6f34602e99391c2489171ee57
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-10-01 12:47:31 +0300

    moved favicon to images folder

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel='icon' href='/favicon.ico'/>
+        <link rel='icon' href='../images/favicon.ico'/>
       </head>
       <body>
         {children}
