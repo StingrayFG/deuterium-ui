ce2117b94a324666d8302255587fbe901ff57761
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-10-01 12:50:32 +0300

    removed the favicon link in layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,9 +5,6 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel='icon' href='../images/favicon.ico'/>
-      </head>
       <body>
         {children}
       </body>
