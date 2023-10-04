6b1ed7bdec605c57e970f9595f47526d19e23d9f
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-10-04 18:27:28 +0300

    changed the imported logo name to camel case

--- app/page.js
+++ app/page.js
@@ -5,15 +5,15 @@ import Link from 'next/link';
 import FileUploadPage from 'components/FileUploadPage'
 
 import githubMark from '/images/github-mark-white.png';
-import Logo from '/images/logo-white.png';
+import logo from '/images/logo-white.png';
 
 export default function Page() {
   return (
     <div className='bg-gradient-to-b from-black/75 via-transparent to-sky-500/75 h-screen grid'>
-      <Image src={Logo} alt='logo' className='absolute top-0 left-0 pl-4 w-16 md:w-24'/>
+      <Image src={logo} alt='logo' className='absolute top-0 left-0 pl-4 w-16 md:w-24'/>
       <FileUploadPage /> 
       <Link href='https://github.com/StingrayFG/' className='absolute bottom-0 left-0 p-6'>
-        <Image src={githubMark} alt='github' className='w-10 md:w-16' />
+        <Image src={githubMark} alt='githubMark' className='w-10 md:w-16' />
       </Link>
     </div>
   )
