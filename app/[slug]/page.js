f8ff1336916506bdda5d9b20943e7c5db1ea2f1a
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-10-10 19:10:06 +0300

    Update page.js

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -10,7 +10,7 @@ import logo from '/images/logo-white.png';
 export default function Page({ params }) {
   return (
     <div className='bg-gradient-to-b from-black/75 via-transparent to-sky-500/75 h-screen grid'>
-      <Image unoptimized  src={logo} alt='logo' className='absolute top-0 left-0 pl-4 pt-2 w-16 md:w-24'/>
+      <Image unoptimized src={logo} alt='logo' className='absolute top-0 left-0 pl-4 pt-2 w-16 md:w-24'/>
       <FilePage slug={params.slug} />
       <Link href='https://github.com/StingrayFG/' className='absolute bottom-0 left-0 p-6'>
         <Image unoptimized src={githubMark} alt='githubMark' className='w-10 md:w-16' />
