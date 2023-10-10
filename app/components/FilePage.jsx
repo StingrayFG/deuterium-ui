a2ccb330d2be6a4e5429f18822db112b59ff0a26
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2023-10-10 13:41:58 +0300

    changed <Link/> to <a/> to avoid possible issues

--- app/components/FilePage.jsx
+++ app/components/FilePage.jsx
@@ -47,13 +47,13 @@ export default function FilePage(props) {
           <div className='w-full h-auto mx-auto sliding-div self-center
             bg-gray-900/50
             border-dashed border-2 border-sky-200 rounded-lg'>
-            <Link href={process.env.NEXT_PUBLIC_BACKEND_URL + '/file/' + props.slug + '/download'}>
+            <a href={process.env.NEXT_PUBLIC_BACKEND_URL + '/file/' + props.slug + '/download'}>
               <div className='w-11/12 h-24 md:h-32 mx-auto mt-8 grid
                 bg-gray-900 hover:bg-gray-800 active:bg-gray-700
                 border-solid border-2 border-sky-700 rounded-lg'>
                 <p className='text-center text-sky-400 font-sans text-2xl md:text-4xl place-self-center'>Download</p>
               </div>
-            </Link>
+            </a>
             <div className='h-14 md:h-16 grid'>
               <p className='pl-6 pr-6 mt-2 text-center align-middle text-sky-400 font-sans text-xl md:text-2xl self-center'>{truncateFileName(fileData.name)} {fileData.size}MB</p>
             </div>
